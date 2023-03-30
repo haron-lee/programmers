edb90c3971b150e7d17cdd8edf0c4454c0340f54
Doha <88657261+haron-lee@users.noreply.github.com>
2023-03-31 00:26:30 +0900

    [level 0] Title: 모스부호 (1), Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,18 @@
+function solution(letter) {
+    const morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+    
+    var answer = '';
+    const splitLetter = letter.split(' ');
+    
+    for(let i = 0; i < splitLetter.length; i++) {
+        answer += morse[splitLetter[i]];
+    }
+    
+    return answer;
+}
