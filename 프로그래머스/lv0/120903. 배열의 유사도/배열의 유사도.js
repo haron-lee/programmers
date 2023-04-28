e1fd0dba25dd620b91c97203e9eb32d690754e6c
Doha Lee <88657261+haron-lee@users.noreply.github.com>
2023-04-28 22:57:11 +0900

    [level 0] Title: 배열의 유사도, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/lv0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,4 +1,5 @@
 function solution(s1, s2) {
-    var answer = s1.filter(el => s2.includes(el));
-    return answer.length;
+    var answer = s1.filter((el) => s2.indexOf(el));
+    
+    return answer;
 }
