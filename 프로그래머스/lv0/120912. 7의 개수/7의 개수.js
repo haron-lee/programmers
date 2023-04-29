69e174bf29cfeb911a10c16eea8a8577c5f05fd7
Doha Lee <88657261+haron-lee@users.noreply.github.com>
2023-04-29 11:25:21 +0900

    [level 0] Title: 7의 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.js
@@ -1,10 +1,3 @@
 function solution(array) {
-    let count = 0;
-    array.forEach((el) => {
-        const str = el.toString(); // 각 요소를 문자열로 변환
-        for (let i = 0; i < str.length; i++) { // 반복문으로 요소의 문자열을 순회
-            if(str[i] === '7') count++; // 7이 나오면 count를 ++ 
-        }
-    })
-    return count
+    return Array.from(array.join('')).filter(v => v === '7').length;
 }
