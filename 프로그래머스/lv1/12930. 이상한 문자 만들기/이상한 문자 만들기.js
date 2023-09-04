53c1b80d070fde96c07d1aeda519a157fc59e860
Doha Lee <88657261+haron-lee@users.noreply.github.com>
2023-09-04 13:05:40 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.27 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -1,17 +1,10 @@
 function solution(s) {
-    let sArr = s.split(' ');
-    
-    const answer = sArr.map((word) => {
-    let newWord = [...word].map((each, index) => {
-      if (index === 0 || index % 2 === 0) {
-        return each.toUpperCase();
-      } else {
-        return each.toLowerCase();
-      }
-    });
-
-    return newWord.join('');
-  });
-    
-    return answer.join(' ');
+  return s
+    .split(' ')
+    .map((word) =>
+      [...word]
+        .map((char, index) => (index % 2 === 0 ? char.toUpperCase() : char.toLowerCase()))
+        .join('')
+    )
+    .join(' ');
 }
