b4d1adc7e4447fee27f8d3412fefc6b163bf8d21
Doha Lee <88657261+haron-lee@users.noreply.github.com>
2023-09-04 12:57:23 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.76 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -2,7 +2,7 @@ function solution(arr){
     var answer = [];
     
     for(let i = 0; i < arr.length; i++) {
-        if(arr[i] !== arr[i+1]) {
+        if(arr[i] !== arr[i - 1]) {
             answer.push(arr[i]);
         }
     }
